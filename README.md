d6745902c8a27ade334841adaa554adc86b511ae
Jeffe <j.garner200@gmail.com>
2020-10-01 20:08:52 -0400

    Code/ markup and styling mostly done for top half

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ A React-based design project to recreate styled flight tickets, with added anima
 
 ## Objectives
 
-- Given a pre-existing image, recreate the design as best as possible through react.
+- Given a pre-existing image (the one seen above), recreate the design as best as possible through react.
 - Add animation features to improve the client-facing experience.
+
+## Process
+
+- Using image editing software, establish and define how many and what kind of colors were used.
+- Using Google Fonts, establish and define how many and what kind of fonts were used.
+- Using image editing software, recreate icons and QR codes.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,89 @@
 import React from "react";
+import "./styles/main.scss";
+
+const ticketStyles = ["Alpha", "OMEGA"];
+
+const ticketInfo = {
+  Alpha: [
+    "Flight 5791 /// Nov. 3, 2018",
+    "Frida Michele Albers",
+    "08:45AM PST",
+    "AA-23",
+    "ALPHA ONE",
+    "ZZ",
+    "2A",
+    "SEA",
+    "Seatac, WA",
+    "LAX",
+    "Los Angeles, CA",
+  ],
+  OMEGA: [
+    "Flight 673 /// Nov. 3, 2018",
+    "Solange Ora",
+    "11:37AM EST",
+    "F-35",
+    "Priority",
+    "3",
+    "23D",
+    "JFK",
+    "New York, NY",
+    "MCO",
+    "Orlando, FL",
+  ],
+};
+
+function renderTickets() {
+  return ticketStyles.map((ticketStyle) => {
+    return (
+      <div className={"ticket " + ticketStyle}>
+        <div className="ticket-top">
+          <div className="ticket-banner">
+            <h1>{ticketStyle}</h1>
+          </div>
+          <div className="flight-info">
+            <p>{ticketInfo[ticketStyle][0]}</p>
+            <div className="info-wrapper">
+              <p>
+                {ticketInfo[ticketStyle][1]} <hr />
+                <label>PASSENGER</label>
+              </p>
+              <div className="info-row">
+                <p>
+                  {ticketInfo[ticketStyle][2]} <hr />
+                  <label>DEPATURE TIME</label>
+                </p>
+                <p>
+                  {ticketInfo[ticketStyle][3]} <hr />
+                  <label>GATE</label>
+                </p>
+              </div>
+              <div className="info-row">
+                <p>
+                  {ticketInfo[ticketStyle][4]} <hr />
+                  <label>CLASS</label>
+                </p>
+                <div className="info-row">
+                  <p>
+                    {ticketInfo[ticketStyle][5]} <hr />
+                    <label>ZONE</label>
+                  </p>
+                  <p>
+                    {ticketInfo[ticketStyle][6]} <hr />
+                    <label>SEAT</label>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="ticket-bottom"></div>
+      </div>
+    );
+  });
+}
 
 function App() {
-  return <div className="App"></div>;
+  return <div id="App">{renderTickets()}</div>;
 }
 
 export default App;
